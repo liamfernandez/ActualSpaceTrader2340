ef733f23f797b98a372d39837ca1f71680bf2436
Liam Fernandez <liamdfernandez@gmail.com>
2019-02-12 22:35:14 -0500

    Retrying uploading files
    Re-uploaded files with correct package names

---
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodels/configurationViewModel.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340.spacetrader.viewmodels;
+
+public class configurationViewModel {
+}
---
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/views/configurationActivity.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340.spacetrader.views;
+
+public class configurationActivity {
+}
