b5ef8f79fa1e4c815a35fe998e5d48619f4c060f
Lauren Prinn <laurenprinn98@gmail.com>
2019-02-22 14:16:15 -0500

    I have created the character classes

---
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/entity/Character.java
@@ -0,0 +1,80 @@
+package edu.gatech.cs2340.spacetrader.entity;
+
+public abstract class Character {
+    String name;
+    int age;
+    boolean evil;
+    int skill1;
+    int skill2;
+    int skill3;
+    int skill4;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isEvil() {
+        return evil;
+    }
+
+    public void setEvil(boolean evil) {
+        this.evil = evil;
+    }
+
+    public int getSkill1() {
+        return skill1;
+    }
+
+    public void setSkill1(int skill1) {
+        this.skill1 = skill1;
+    }
+
+    public int getSkill2() {
+        return skill2;
+    }
+
+    public void setSkill2(int skill2) {
+        this.skill2 = skill2;
+    }
+
+    public int getSkill3() {
+        return skill3;
+    }
+
+    public void setSkill3(int skill3) {
+        this.skill3 = skill3;
+    }
+
+    public int getSkill4() {
+        return skill4;
+    }
+
+    public void setSkill4(int skill4) {
+        this.skill4 = skill4;
+    }
+
+    public Character(String name, boolean evil, int skill1, int skill2, int skill3, int skill4, int age) {
+        this.name = name;
+        this.age = age;
+        this.evil = evil;
+        this.skill1 = skill1;
+        this.skill2 = skill2;
+        this.skill3 = skill3;
+        this.skill4 = skill4;
+    }
+    public Character(String name, int skill1, int skill2, int skill3, int skill4) {
+        this(name, false, skill1, skill2, skill3, skill4, 50);
+    }
+}
