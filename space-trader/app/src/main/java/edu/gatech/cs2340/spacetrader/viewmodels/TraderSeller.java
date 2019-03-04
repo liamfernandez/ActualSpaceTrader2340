c7d3a868351cbc23d601bf72730831be90a082b0
McKay <bmckaym@gmail.com>
2019-03-03 22:22:10 -0500

    Added lots of javadocs

--- space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodels/TraderSeller.java
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodels/TraderSeller.java
@@ -4,10 +4,12 @@ import edu.gatech.cs2340.spacetrader.entity.Planet;
 import edu.gatech.cs2340.spacetrader.model.Store;
 
 public class TraderSeller extends DisplayableSeller {
-    private Planet planet;
 
-    public TraderSeller(Planet p) {
-        planet = p;
+    /**
+     * Populates trader's list of items for sale
+     * using the Store class
+     */
+    public TraderSeller() {
         itemsMarketSells = Store.getTraderItems();
     }
 }
