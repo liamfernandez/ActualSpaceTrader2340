10029b83ba9f9cb94ab7e94d16353396fc0ff7e4
Liam Fernandez <liamfernandez@c-24-72-148-84.ni.gigamonster.net>
2019-03-03 21:37:13 -0500

    Added hasItem and implemented remove method

--- space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodels/DisplayableSeller.java
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodels/DisplayableSeller.java
@@ -3,17 +3,26 @@ package edu.gatech.cs2340.spacetrader.viewmodels;
 import java.util.HashMap;
 
 import edu.gatech.cs2340.spacetrader.entity.Item;
+import android.util.Pair;
 
 public abstract class DisplayableSeller {
-    private HashMap<Item, Integer> itemsMarketSells;
+    private HashMap<Item, Pair<Integer, Double>> itemsMarketSells;
 
-    public abstract HashMap<Item, Integer> stockSeller();
+    public abstract HashMap<Item, Pair<Integer, Double>> stockSeller();
 
-    public HashMap<Item, Integer> getItemsSold() {
+    public HashMap<Item, Pair<Integer, Double>> getItemsSold() {
         return itemsMarketSells;
     }
 
     public void remove(Item s) {
-        
+        Pair<Integer, Double> oldPair = itemsMarketSells.get(s);
+        Integer newSupplyLevel = oldPair.first;
+        newSupplyLevel--;
+        Pair<Integer, Double> newPair = new Pair<>(newSupplyLevel, oldPair.second);
+        itemsMarketSells.replace(s, newPair);
+    }
+
+    public boolean hasItem(Item s) {
+        return itemsMarketSells.get(s).first > 0;
     }
 }
