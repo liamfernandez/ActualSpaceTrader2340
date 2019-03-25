79147c5a3379b40b84bab585c5a374a18459de80
McKay <bmckaym@gmail.com>
2019-03-25 18:52:17 -0400

    m

--- space-trader/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ space-trader/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080027;
-        public static final int end = 0x7f080047;
-        public static final int gone = 0x7f080055;
-        public static final int invisible = 0x7f08005f;
-        public static final int left = 0x7f080065;
-        public static final int packed = 0x7f080079;
-        public static final int parent = 0x7f08007b;
-        public static final int percent = 0x7f08007e;
-        public static final int right = 0x7f080083;
-        public static final int spread = 0x7f0800aa;
-        public static final int spread_inside = 0x7f0800ab;
-        public static final int start = 0x7f0800b0;
-        public static final int top = 0x7f0800d4;
-        public static final int wrap = 0x7f0800e3;
+        public static final int end = 0x7f080049;
+        public static final int gone = 0x7f080057;
+        public static final int invisible = 0x7f080061;
+        public static final int left = 0x7f080067;
+        public static final int packed = 0x7f08007b;
+        public static final int parent = 0x7f08007d;
+        public static final int percent = 0x7f080080;
+        public static final int right = 0x7f080086;
+        public static final int spread = 0x7f0800af;
+        public static final int spread_inside = 0x7f0800b0;
+        public static final int start = 0x7f0800b5;
+        public static final int top = 0x7f0800d9;
+        public static final int wrap = 0x7f0800e8;
     }
     public static final class styleable {
         private styleable() {}
