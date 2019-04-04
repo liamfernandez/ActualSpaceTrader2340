84bbbbd1d710587c34452e189a70f6215209223c
Liam Fernandez <liamfernandez@lawn-128-61-79-131.lawn.gatech.edu>
2019-04-04 16:31:21 -0400

    added java docs to Player class

--- space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/entity/Player.java
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/entity/Player.java
@@ -25,6 +25,10 @@ public class Player extends Character{
         //solarSystem = new SolarSystem(null, 0, 0,"initial");
     }
 
+    /**
+     * to string
+     * @return the string
+     */
     @Override
     public String toString() {
         return name + " has " + skill1 + " points in skill 1, " + skill2
@@ -40,70 +44,140 @@ public class Player extends Character{
         this.id = id;
     }
 
+
+    /**
+     * Setter
+     * @param name what to set it to
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+
+    /**
+     * Set skill 1
+     * @param skill1 the skill
+     */
     public void setSkill1(int skill1) {
         this.skill1 = skill1;
     }
 
+    /**
+     * Set skill 2
+     * @param skill2 skill 2 to set it to
+     */
     public void setSkill2(int skill2) {
         this.skill2 = skill2;
     }
 
+    /**
+     * setter for skill 3
+     * @param skill3 the skill
+     */
     public void setSkill3(int skill3) {
         this.skill3 = skill3;
     }
 
+    /**
+     * Setter for skill 4
+     * @param skill4 the skill
+     */
     public void setSkill4(int skill4) {
         this.skill4 = skill4;
     }
 
+    /**
+     * getter for name
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * the get skill
+     * @return skill 1
+     */
     public int getSkill1() {
         return skill1;
     }
 
+    /**
+     * skill 2 getter
+     * @return skill 2
+     */
     public int getSkill2() {
         return skill2;
     }
 
+    /**
+     *
+     * @return skill 3
+     */
     public int getSkill3() {
         return skill3;
     }
 
+    /**
+     *
+     * @return skill 4
+     */
     public int getSkill4() {
         return skill4;
     }
 
+    /**
+     *
+     * @return the curr planet
+     */
     public Planet getCurrPlanet() {
         return currPlanet;
     }
 
+    /**
+     *
+     * @return the inventory object of the player
+     */
     public Inventory getInventory() {
         return inventory;
     }
 
+    /**
+     * setter for inventory
+     * @param inventory
+     */
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
     }
 
+    /**
+     *
+     * @return cargo total
+     */
     public int getCargoTotal() {
         return cargoTotal;
     }
 
+    /**
+     *
+     * @return the max items
+     */
     public int getMaxItems() {
         return maxItems;
     }
 
+    /**
+     *
+     * @return the current solar system
+     */
     public SolarSystem getCurrSolarSystem() {
         return currSolarSystem;
     }
 
+    /**
+     *
+     * @param solarSystem set the system
+     */
     public void setCurrSolarSystem(SolarSystem solarSystem) {
         this.currSolarSystem = solarSystem;
     }
@@ -143,10 +217,18 @@ public class Player extends Character{
         inventory.add(item);
     }
 
+    /**
+     * Getter for Fuel
+     * @return the amount of Fuel
+     */
     public double getFuel() {
         return fuel;
     }
 
+    /**
+     * Subtracts fuel when traveling
+     * @param subtract the amount to subtract
+     */
     public void subtractFuel(double subtract) {
         this.fuel = fuel - subtract;
     }
