f05e2ff779e31fa778b5f3b157764abe2edf9bfc
Liam Fernandez <liamfernandez@lawn-128-61-79-131.lawn.gatech.edu>
2019-04-04 17:31:45 -0400

    Java docced Character class

--- space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/entity/Character.java
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/entity/Character.java
@@ -1,5 +1,8 @@
 package edu.gatech.cs2340.spacetrader.entity;
 
+/**
+ *
+ */
 public abstract class Character {
     String name;
 //    int age;
@@ -9,10 +12,18 @@ public abstract class Character {
     int skill3;
     int skill4;
 
+    /**
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @param name name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -25,38 +36,78 @@ public abstract class Character {
 //        this.evil = evil;
 //    }
 
+    /**
+     *
+     * @return the skill1
+     */
     public int getSkill1() {
         return skill1;
     }
 
+    /**
+     *
+     * @param skill1 skill 1
+     */
     public void setSkill1(int skill1) {
         this.skill1 = skill1;
     }
 
+    /**
+     *
+     * @return skill 2
+     */
     public int getSkill2() {
         return skill2;
     }
 
+    /**
+     *
+     * @param skill2 skill 2
+     */
     public void setSkill2(int skill2) {
         this.skill2 = skill2;
     }
 
+    /**
+     *
+     * @return skill 3
+     */
     public int getSkill3() {
         return skill3;
     }
 
+    /**
+     *
+     * @param skill3 skill 3
+     */
     public void setSkill3(int skill3) {
         this.skill3 = skill3;
     }
 
+    /**
+     *
+     * @return skill 4
+     */
     public int getSkill4() {
         return skill4;
     }
 
+    /**
+     *
+     * @param skill4 skill 4
+     */
     public void setSkill4(int skill4) {
         this.skill4 = skill4;
     }
 
+    /**
+     * Constructor for character
+     * @param name the name
+     * @param skill1 skill 1
+     * @param skill2 skill 2
+     * @param skill3 skill 3
+     * @param skill4 skill 4
+     */
     public Character(String name, int skill1, int skill2, int skill3, int skill4) {
         this.name = name;
         //this.evil = evil;
