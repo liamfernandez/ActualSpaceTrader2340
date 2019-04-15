c0507bce018a96ce1f14e1d87119ba9df98e7a70
McKay <bmckaym@gmail.com>
2019-04-14 21:57:39 -0400

    commit

--- space-trader/app/src/test/java/edu/gatech/cs2340/spacetrader/UnitTests.java
+++ space-trader/app/src/test/java/edu/gatech/cs2340/spacetrader/UnitTests.java
@@ -2,14 +2,25 @@ package edu.gatech.cs2340.spacetrader;
 
 import android.app.Application;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import edu.gatech.cs2340.spacetrader.entity.Player;
+import edu.gatech.cs2340.spacetrader.model.Model;
+import edu.gatech.cs2340.spacetrader.model.Repository;
 import edu.gatech.cs2340.spacetrader.viewmodels.SignInViewModel;
 
 public class UnitTests {
+
+    Repository repository;
+
+    @Before
+    public void setUp() {
+        repository = Model.getInstance().getMarketInteractor().getRepository();
+    }
+
     @Test
     public void testValidatePlayer() {
         SignInViewModel s = new SignInViewModel(new Application());
@@ -27,5 +38,11 @@ public class UnitTests {
         assertEquals("it should be okay if only one is 16 and the rest are 0", true, s.validateTest(justRightSkills3));
         assertEquals("it should be okay if only one is 16 and the rest are 0", true, s.validateTest(justRightSkills4));
         assertEquals("it should be okay if all add up to 16", true, s.validateTest(justRightSum));
+    }
+
+    @Before
+    public void testDownloadPlayer() {
+        String[] existingPlayers = {"MATT","p","porter","qwe"};
+        String[] nonexistantPlayers = {"noPlayer1", "noPlayer2", "noPlayer3", "noPlayer4"};
     }
 }
