279b3ae27f061b58c09a452ede1853e35315a3ef
Liam Fernandez <liamfernandez@c-24-72-151-87.ni.gigamonster.net>
2019-04-17 02:26:29 -0400

    Some intro beautification

--- space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/views/MainActivity.java
+++ space-trader/app/src/main/java/edu/gatech/cs2340/spacetrader/views/MainActivity.java
@@ -1,15 +1,18 @@
 package edu.gatech.cs2340.spacetrader.views;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuInflater;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import edu.gatech.cs2340.spacetrader.R;
 import edu.gatech.cs2340.spacetrader.model.MySQLTalker;
@@ -27,8 +30,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button start = findViewById(R.id.PlayGameBtn);
         Button returningPlayer = findViewById(R.id.returningPlayer);
-
-        MySQLTalker.initialize();
+        final ImageView background1 = findViewById(R.id.appCompatImageView1);
+        final ImageView background2 = findViewById(R.id.appCompatImageView2);
+        final ValueAnimator animate = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animate.setRepeatCount(ValueAnimator.INFINITE);
+        animate.setInterpolator(new LinearInterpolator());
+        animate.setDuration(12500L);
+        animate.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = background1.getWidth();
+                final float translationX = width * progress;
+                background1.setTranslationX(translationX);
+                background2.setTranslationX(translationX - width);
+            }
+        });
+        animate.start();
+        //MySQLTalker.initialize();
 
         start.setOnClickListener(e -> {
                 Intent intent = new Intent(MainActivity.this, SignInActivity.class);
